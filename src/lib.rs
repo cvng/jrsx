6cb45b53b95b13a29a132a97d57844dd5851cac1
Cedric Vangout <mail@cvng.dev>
2024-01-10 23:53:37 +0100

    feat: parse macro attrs (#1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,44 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use std::env;
+use std::fs;
+use syn::meta::ParseNestedMeta;
+use syn::parse::Result;
+use syn::parse_macro_input;
+use syn::DeriveInput;
+use syn::LitStr;
 
 #[proc_macro_attribute]
-pub fn template(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let attr = attr.to_string();
-    let item = item.to_string();
+pub fn template(args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut attrs = TemplateAttributes::default();
+    let template_parser = syn::meta::parser(|meta| attrs.parse(meta));
+    parse_macro_input!(args with template_parser);
+    let input = parse_macro_input!(input as DeriveInput);
 
-    format!(
-        r#"
+    let path = attrs.path.unwrap().value();
+    let path = env::current_dir().unwrap().join("templates").join(path);
+    let source = fs::read_to_string(&path).unwrap();
+
+    quote! {
         #[derive(::askama::Template)]
-        #[template({attr})]
-        {item}
-        "#
-    )
-    .parse()
-    .unwrap()
+        #[template(source = #source, ext = "html")]
+        #input
+    }
+    .into()
+}
+
+#[derive(Default)]
+struct TemplateAttributes {
+    path: Option<LitStr>,
+}
+
+impl TemplateAttributes {
+    fn parse(&mut self, meta: ParseNestedMeta) -> Result<()> {
+        if meta.path.is_ident("path") {
+            self.path = meta.value()?.parse()?;
+            Ok(())
+        } else {
+            Err(meta.error("unsupported template property"))
+        }
+    }
 }
