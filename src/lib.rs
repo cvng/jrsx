2559238227a55988b508a4b2f61f4db88ceef923
Cedric Vangout <mail@cvng.dev>
2024-01-10 23:51:15 +0100

    read_file

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use proc_macro::TokenStream;
 use quote::quote;
+use std::env;
+use std::fs;
 use syn::meta::ParseNestedMeta;
 use syn::parse::Result;
 use syn::parse_macro_input;
@@ -8,17 +10,18 @@ use syn::LitStr;
 
 #[proc_macro_attribute]
 pub fn template(args: TokenStream, input: TokenStream) -> TokenStream {
+    let mut attrs = TemplateAttributes::default();
+    let template_parser = syn::meta::parser(|meta| attrs.parse(meta));
+    parse_macro_input!(args with template_parser);
     let input = parse_macro_input!(input as DeriveInput);
 
-    let mut attrs = TemplateAttributes::default();
-    let tpl_parser = syn::meta::parser(|meta| attrs.parse(meta));
-    parse_macro_input!(args with tpl_parser);
-
     let path = attrs.path.unwrap().value();
+    let path = env::current_dir().unwrap().join("templates").join(path);
+    let source = fs::read_to_string(&path).unwrap();
 
     quote! {
         #[derive(::askama::Template)]
-        #[template(path = #path)]
+        #[template(source = #source, ext = "html")]
         #input
     }
     .into()
