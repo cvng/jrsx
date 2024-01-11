9faed8f287ebd9cb1f7df41f64e9977f7c860d24
Cedric Vangout <mail@cvng.dev>
2024-01-11 20:45:04 +0100

    feat: jsx-like macro (#3)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use regex::Regex;
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 use syn::meta::ParseNestedMeta;
@@ -8,6 +9,9 @@ use syn::parse::Result;
 use syn::parse_macro_input;
 use syn::DeriveInput;
 use syn::LitStr;
+
+// TODO: https://crates.io/crates/syn-rsx
+const COMPONENT_RE: &str = r#"<([A-Z][a-zA-Z0-9]*)\s*([^>/]*)\s*/*?>"#;
 
 #[derive(Default)]
 struct TemplateAttributes {
@@ -34,8 +38,8 @@ pub fn template(args: TokenStream, input: TokenStream) -> TokenStream {
 
     let path = attrs.path.unwrap().value();
     let path = env::current_dir().unwrap().join("templates").join(path);
-    let source = fs::read_to_string(path).unwrap();
-    let source = rewrite_source(&source);
+    let source = fs::read_to_string(&path).unwrap();
+    let source = rewrite_source(path.file_stem().unwrap().to_str().unwrap(), source);
 
     quote! {
         #[derive(::askama::Template)]
@@ -45,11 +49,36 @@ pub fn template(args: TokenStream, input: TokenStream) -> TokenStream {
     .into()
 }
 
-const COMPONENT_RE: &str = r#"<([A-Z][a-zA-Z0-9]*)\s*([^>/]*)\s*/*?>"#;
-
-fn rewrite_source(source: &str) -> String {
+fn rewrite_source(name: &str, source: String) -> String {
     let re = Regex::new(COMPONENT_RE).unwrap();
-    re.replace_all(source, rewrite_component).into_owned()
+    let import = add_import(re.captures_iter(&source));
+    let source = re.replace_all(&source, rewrite_component).into_owned();
+
+    format!(
+        "\
+        {import}\n\
+        {{% macro {name}() %}}\n\
+        {source}\n\
+        {{% endmacro %}}\n\
+        {{% call {name}() %}}",
+    )
+}
+
+fn add_import(caps: regex::CaptureMatches) -> String {
+    let mut import = HashSet::new();
+    let mut output = String::new();
+
+    for cap in caps {
+        let name = cap.get(1).unwrap().as_str().to_ascii_lowercase();
+        import.insert(name);
+    }
+
+    for name in import {
+        let line = format!("{{%- import \"{name}.html\" as {name}_scope -%}}\n");
+        output.push_str(&line);
+    }
+
+    output
 }
 
 fn rewrite_component(caps: &regex::Captures) -> String {
@@ -62,19 +91,18 @@ fn rewrite_component(caps: &regex::Captures) -> String {
         .collect::<Vec<_>>()
         .join(", ");
 
-    format!(
-        "\
-        {{%- import \"{name}.html\" as {name}_scope -%}}\n\
-        {{% call {name}_scope::{name}({args}) %}}",
-    )
+    format!("{{% call {name}_scope::{name}({args}) %}}")
 }
 
 #[test]
 fn test_rewrite_source() {
     assert_eq!(
-        rewrite_source("<Hello name />"),
+        rewrite_source("index", "<Hello name />".to_string()),
         "\
-        {%- import \"hello.html\" as hello_scope -%}\n\
-        {% call hello_scope::hello(name) %}"
+        {%- import \"hello.html\" as hello_scope -%}\n\n\
+        {% macro index() %}\n\
+        {% call hello_scope::hello(name) %}\n\
+        {% endmacro %}\n\
+        {% call index() %}"
     );
 }
