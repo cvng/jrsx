eee8afa875f4b9d66196bda1e90a821aed649b0f
Cedric Vangout <mail@cvng.dev>
2024-01-13 04:30:41 +0100

    feat: use parser (#9)

--- derive/src/rewrite.rs
+++ derive/src/rewrite.rs
@@ -1,30 +1,19 @@
-#![allow(dead_code)]
-
 use once_cell::sync::Lazy;
 use regex::Regex;
 use std::collections::HashSet;
 use std::path::Path;
 
-static JSX_BLOCK_RE: Lazy<Regex> = Lazy::new(|| {
-    let re = Regex::new(r#"<([A-Z][a-zA-Z0-9]*)\s*([^>/]*)\s*/*?>"#);
-    re.unwrap()
-});
-
-static MACRO_DEF_RE: Lazy<Regex> = Lazy::new(|| {
-    let re = Regex::new(r#"\{#def\s+(.+)\s+#\}"#);
-    re.unwrap()
-});
-
 static SYNTAX_RE: Lazy<Regex> = Lazy::new(|| {
     Regex::new(&format!(
-        "({}|{}|{})",
+        r#"({}|{}|{})"#,
         r#"(?<jsx><([A-Z][a-zA-Z0-9]*)\s*([^>/]*)\s*/*?>)"#, // <Hello name />
-        r#"(?<def>\{#def\s+(.+)\s+#\})"#,                    //  {#def name #}
-        r#"(?<src>[\w+\s+]*)"#,
+        r#"(?<def>\{#def\s+(.+)\s+#\})"#,                    // {#def name #}
+        r#"(?<src>.*[\w+\s+]*)"#,
     ))
     .unwrap()
 });
 
+#[derive(Debug, PartialEq)]
 struct Ast {
     nodes: Vec<Node>,
 }
@@ -44,44 +33,7 @@ struct JsxBlock {
 
 #[derive(Debug, PartialEq)]
 struct MacroDef {
-    name: String,
     args: Vec<String>,
-}
-
-fn parsed(source: &str) -> Ast {
-    let mut nodes = Vec::new();
-
-    for caps in SYNTAX_RE.captures_iter(source) {
-        match caps {
-            caps if caps.name("jsx").is_some() => {
-                dbg!("jsx", &caps);
-                let name = caps[3].to_owned();
-                let args = caps[4]
-                    .split_ascii_whitespace()
-                    .map(|s| s.to_owned())
-                    .collect();
-
-                nodes.push(Node::JsxBlock(JsxBlock { name, args }));
-            }
-            caps if caps.name("def").is_some() => {
-                dbg!("def", &caps);
-                let name = caps[1].to_owned();
-                let args = caps[2]
-                    .split_ascii_whitespace()
-                    .map(|s| s.to_owned())
-                    .collect();
-
-                nodes.push(Node::MacroDef(MacroDef { name, args }));
-            }
-            caps if caps.name("src").is_some() => {
-                dbg!("src", &caps);
-                nodes.push(Node::Source(caps[7].to_owned()));
-            }
-            _ => unreachable!(),
-        }
-    }
-
-    Ast { nodes }
 }
 
 pub(crate) fn rewrite_path<P>(path: P) -> String
@@ -99,21 +51,78 @@ where
     P: AsRef<Path>,
 {
     let mut buf = String::with_capacity(source.capacity());
+
+    let ast = parsed(source);
     let macro_name = normalize(path);
 
-    write_imports(&mut buf, &source);
-    write_macro_def(&mut buf, &source, &macro_name);
-    write_macro_body(&mut buf, source);
-    write_macro_end(&mut buf, &macro_name);
+    visit_ast(&mut buf, &ast, &macro_name);
 
     buf
 }
 
-fn write_imports(buf: &mut String, source: &str) {
+fn parsed(source: String) -> Ast {
+    let mut nodes = Vec::new();
+
+    for caps in SYNTAX_RE.captures_iter(&source) {
+        match caps {
+            caps if caps.name("jsx").is_some() => {
+                let name = caps[3].to_owned();
+                let args = caps[4]
+                    .split_ascii_whitespace()
+                    .map(|s| s.to_owned())
+                    .collect();
+
+                nodes.push(Node::JsxBlock(JsxBlock { name, args }));
+            }
+            caps if caps.name("def").is_some() => {
+                let args = caps[6]
+                    .split_ascii_whitespace()
+                    .map(|s| s.to_owned())
+                    .collect();
+
+                nodes.push(Node::MacroDef(MacroDef { args }));
+            }
+            caps if caps.name("src").is_some() => {
+                nodes.push(Node::Source(caps[7].to_owned()));
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    Ast { nodes }
+}
+
+fn visit_ast(buf: &mut String, ast: &Ast, macro_name: &str) {
+    write_imports(
+        buf,
+        &ast.nodes
+            .iter()
+            .filter_map(|node| match node {
+                Node::JsxBlock(node) => Some(node),
+                _ => None,
+            })
+            .collect::<Vec<_>>(),
+    );
+
+    write_macro_def(
+        buf,
+        macro_name,
+        ast.nodes.iter().find_map(|node| match node {
+            Node::MacroDef(node) => Some(node),
+            _ => None,
+        }),
+    );
+
+    visit_nodes(buf, &ast.nodes);
+
+    write_macro_end(buf, macro_name);
+}
+
+fn write_imports(buf: &mut String, blocks: &[&JsxBlock]) {
     let mut imports = HashSet::new();
 
-    for caps in JSX_BLOCK_RE.captures_iter(source) {
-        let macro_name = normalize(&caps[1]);
+    for block in blocks {
+        let macro_name = normalize(&block.name);
         let macro_import = format!("{macro_name}.html");
 
         if imports.insert(macro_name.clone()) {
@@ -124,11 +133,8 @@ fn write_imports(buf: &mut String, source: &str) {
     }
 }
 
-fn write_macro_def(buf: &mut String, source: &str, macro_name: &str) {
-    let macro_args = match MACRO_DEF_RE.captures(source) {
-        Some(caps) => caps[1].to_owned(),
-        None => String::new(),
-    };
+fn write_macro_def(buf: &mut String, macro_name: &str, macro_def: Option<&MacroDef>) {
+    let macro_args = macro_def.map(|m| m.args.join(", ")).unwrap_or_default();
 
     buf.push_str(&format!("{{% macro {macro_name}({macro_args}) %}}\n"));
 }
@@ -137,21 +143,27 @@ fn write_macro_end(buf: &mut String, macro_name: &str) {
     buf.push_str(&format!("{{% endmacro {macro_name} %}}\n"));
 }
 
-fn write_macro_body(buf: &mut String, source: String) {
-    let source = MACRO_DEF_RE.replace_all(&source, "");
-    let source = JSX_BLOCK_RE.replace_all(&source, replace_macro_call);
-
-    buf.push_str(&source);
+fn visit_nodes(buf: &mut String, nodes: &[Node]) {
+    for node in nodes {
+        match node {
+            Node::JsxBlock(node) => {
+                write_macro_call(buf, node);
+            }
+            Node::Source(source) => {
+                buf.push_str(source);
+            }
+            _ => {}
+        }
+    }
 }
 
-fn replace_macro_call(caps: &regex::Captures<'_>) -> String {
-    let macro_name = normalize(&caps[1]);
-    let macro_args = caps[2]
-        .split_ascii_whitespace()
-        .collect::<Vec<_>>()
-        .join(", ");
+fn write_macro_call(buf: &mut String, block: &JsxBlock) {
+    let macro_name = normalize(&block.name);
+    let macro_args = block.args.join(", ");
 
-    format!("{{% call {macro_name}_scope::{macro_name}({macro_args}) %}}\n")
+    buf.push_str(&format!(
+        "{{% call {macro_name}_scope::{macro_name}({macro_args}) %}}\n"
+    ));
 }
 
 fn normalize<P>(path: P) -> String
@@ -184,25 +196,24 @@ fn test_normalize() {
     assert_eq!(normalize("templates/hello_world.html"), "hello_world");
     assert_eq!(normalize("templates/hello-world.html"), "hello_world");
     assert_eq!(normalize("templates/hello.world.html"), "hello_world");
-    // TODO: assert_eq!(normalize("templates/HelloWorld.html"), "hello_world");
 }
 
 #[test]
 fn test_parsed() {
-    assert_eq!(parsed("<Hello name />").nodes.len(), 1);
+    assert_eq!(parsed("<Hello name />".into()).nodes.len(), 1);
     assert_eq!(
-        parsed("<Hello name />").nodes.first(),
+        parsed("<Hello name />".into()).nodes.first(),
         Some(&Node::JsxBlock(JsxBlock {
             name: "Hello".into(),
             args: vec!["name".into()],
         }))
     );
     assert_eq!(
-        parsed("Test\n<Hello name />").nodes.first(),
+        parsed("Test\n<Hello name />".into()).nodes.first(),
         Some(&Node::Source("Test\n".into()))
     );
     assert_eq!(
-        parsed("<Hello name />\nTest").nodes.last(),
+        parsed("<Hello name />\nTest".into()).nodes.last(),
         Some(&Node::Source("\nTest".into()))
     );
 }
