264ded08a0bd1f41671ecab8139e50ba88dd9f10
Cedric Vangout <mail@cvng.dev>
2024-01-14 02:27:44 +0100

    feat: support "caller()"

--- lib/tests/template.rs
+++ lib/tests/template.rs
@@ -1,27 +1,10 @@
 use jrsx::Template;
 
 #[derive(Template)]
-#[template(path = "index.html")]
-struct Index<'a> {
-    name: &'a str,
-}
-
-#[test]
-fn test_template() {
-    assert_eq!(
-        Index { name: "world" }.to_string(),
-        "\n\n\n\n\
-        <h1>Hello, world!</h1>\n\n\n\n\
-        <h1>Hello, world!</h1>\n\n\n\n\
-        <h1>Hello, world!</h1>\n"
-    );
-}
-
-#[derive(Template)]
-#[template(path = "index2.html")]
+#[template(source = "{# call caller() #}", ext = "html")]
 struct Index2 {}
 
 #[test]
 fn test_template2() {
-    assert_eq!(Index2 {}.to_string(), "\n\n<div></div>\n");
+    assert_eq!(Index2 {}.to_string(), "");
 }
